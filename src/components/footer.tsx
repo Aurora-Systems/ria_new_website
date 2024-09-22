58c0a43da2b6445a80b7a621d538c85701ecf5d5
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2024-09-22 18:09:09 +0200

    changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,21 +2,21 @@ const Footer=()=>{
     return(
         <div className="container-fluid text-white">
 
-        <div className="bg-dark row p-2">
-            <div className="col-sm d-flex flex-column text-center">
+        <div className="bg-dark row p-2 d-flex align-items-center">
+            <div className="col-sm mb-3 d-flex flex-column text-center">
                 <p className="fw-bold">Address</p>
                 <span>178 Smuts Rd Waterfalls</span>
                 <span>Harare</span>
                 <span>Zimbabwe</span>
             </div>
-            <div className="col-sm d-flex flex-column text-center">
+            <div className="col-sm mb-3 d-flex flex-column text-center ">
                 <p className="fw-bold">Contact Details</p>
-                <span>+263 77 270 3234</span>
-                <span>admin@ria.co.zw</span>
-                <span>Facebook</span>
+                <a className="text-white" href="tel:+263772703234" target="_blank"><span>+263 77 270 3234</span></a>
+                <a className="text-white" href="mailto:admin@ria.co.zw" target="_blank"><span>admin@ria.co.zw</span></a>
+                <a className="text-white" href="https://www.facebook.com/rhapsodyinteracademy" target="_blank"><span>Facebook</span></a>
             </div>
-            <div className="col-sm rounded">
-            <iframe className="rounded" src="https://rhapsodyinternationalacademy.substack.com/embed"  frameBorder="0" ></iframe>
+            <div className="col-sm mb-3 rounded">
+            <iframe className="rounded bg-dark" src="https://rhapsodyinternationalacademy.substack.com/embed"  ></iframe>
             </div>
         </div>
         </div>
